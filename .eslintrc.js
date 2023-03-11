09329e39e505f07703f208653f277d57b8d60afe
Macanthony <macintony1@gmail.com>
2023-03-11 13:43:41 +0100

    install required packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,23 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  "extends": "plugin:react/recommended",
+  "parser": "@babel/eslint-parser",
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "strict": 0,
+    "no-whitespace-before-property": 2,
+    "no-unused-vars": 1,
+    "react/prop-types": 0,
+    "react/jsx-key": 1,
+    "react/react-in-jsx-scope": 0,
+    "react/display-name": 0,
+    "no-unreachable": 1,
+    "use-isnan": 1,
+    "dot-notation": 1,
+    "eqeqeq": 2,
+    "no-extra-bind": 2,
+    "no-empty-function": 1,
+    "no-restricted-imports": 0
+  }
 };
